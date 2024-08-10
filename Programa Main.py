6ede9fb0db4628d6ccbe4a6d1a72527734a17ef7
sergio <sdelacorreia@correo.unimet.edu.ve>
2024-08-10 16:28:55 -0400

    Sergio Main

--- Programa Main.py
+++ Programa Main.py
@@ -29,10 +29,6 @@ def menu_principal():
             msvcrt.getch()
         elif seleccion == '3':
             limpiarp.limpiar_pantalla()
-            print("Por favor esperar a que termine el proceso...")
-            subprocess.run(["python", ruta+"/Sergio SWAPI(A).py"])
-            print("Presione cualquier tecla para continuar...")
-            msvcrt.getch()
         elif seleccion == '4':
             limpiarp.limpiar_pantalla()
             print("Por favor esperar a que termine el proceso...")
@@ -40,7 +36,7 @@ def menu_principal():
             print("Presione cualquier tecla para continuar...")
             msvcrt.getch()
         elif seleccion == '0':
-            print("¡Hasta luego!")
+            print("\n¡Hasta luego!\n")
             break
         else:
             print("Opción no válida. Inténtalo de nuevo.")
