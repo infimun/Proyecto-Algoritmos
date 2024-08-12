da0c9c2658eed44c25b1fc80b8eb25b34b904a14
sergio <sdelacorreia@correo.unimet.edu.ve>
2024-08-11 22:37:12 -0400

    Center con lista_planetas

--- center.py
+++ center.py
@@ -138,4 +138,92 @@ def seleccionar_especie():
             except ValueError:
                 print("Entrada no válida. Por favor, introduce un número o 'q' para salir.")
 
+"""
+Selector de planetas para el main
+recorrer paginas de la api para ver todos los planetas
+"""
+
+def listar_planetas():
+    current_page = 1
+    planets_data = []
+    total_records = 0
+    planets_per_page = 10
+
+    while True:
+        if not planets_data:
+            response = requests.get(f'https://swapi.py4e.com/api/planets/?page={current_page}&limit={planets_per_page}')
+            if response.status_code == 200:
+                response_data = response.json()
+                planets_data = response_data['results']
+                total_records = response_data['count']
+                total_page = (total_records + planets_per_page - 1) // planets_per_page #Redonde hacia arriba
+            else:
+                print("Error al obtener los datos de planetas")
+                break
+        
+        start_index = (current_page - 1) * planets_per_page + 1
+
+        print('\n Planetas disponibles')
+
+        for index, planet in enumerate(planets_data, start = start_index):
+            print(f'{index}. {planet['name']}')
+
+        #Opciones de navegacion
+        options = "\n Selecciona un planeta por número"
+        if current_page > 1:
+            options += " o 'next' para ver mas opciones"
+        options += " (o 'q' Para volver al menú principal): "
+        choice = input(options)
+        if choice.lower() == 'q':
+            break
+        elif choice.lower() == 'next' and current_page < total_page:
+            current_page += 1 
+            planets_data = []
+        elif choice.lower() == 'prev' and current_page > 1: 
+            current_page += 1
+            planets_data = []
+        else:
+            try:
+                choice = int(choice) - start_index
+                if 0 <= choice < len(planets_data):
+                    planet = planets_data[choice]
+                    planet_name = planet['name']
+                    
+                    #Obtener los episodos en los que aparece el planeta
+                    films = planet.get('films', [])
+                    film_titles = []
+
+                    for film_url in films:
+                        film_response = requests.get(film_url)
+                        if film_response.status_code == 200:
+                            film_title = film_response.json()['title']
+                            film_titles.append(film_title)
+                        else:
+                            film_titles.append("Error al obtener el titulo del episodio")
+
+                    #Mostrar los detalles del planeta y los episodios en los que aparece
+                    print(f"\n Detalles para{planet_name}: ")
+                    print(f"Periodo de orbita: {planet.get('orbital_period', 'N/A')} Dias")
+                    print(f"Periodo de rotacion: {planet.get('rotation_period', 'N/A')} Horas")
+                    print(f"Cantidad de habitantes: {planet.get('population'), ' N/A'}")
+                    print(f"Tipo declima: {planet.get('climate', 'N/A')}")
+
+                    print("\n Episodios en los que aparece:")
+                    if film_titles:
+                        for title in film_titles:
+                            print(f" - {title}")
+                    else:
+                        print("No ha aparecido en ningun episodio")
+                    print() #Espacio
+
+                else:
+                    print("Seleccion invalida. Intentelo de nuevo")
+            except ValueError:
+                print("Entrada no valida. Por favor, introduzca un numero o 'q' para salir") 
+
+                
+                    
+
+
+
 
