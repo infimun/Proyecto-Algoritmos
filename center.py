6241ecda8e8fe818d1c504de9003d3ec0076f6c3
Marco <m.marino@correo.unimet.edu.ve>
2024-08-12 15:18:16 -0400

    acomodado ubicacion del cvs

--- center.py
+++ center.py
@@ -1,7 +1,4 @@
 import requests
-import pandas as pd
-import csv
-import os 
 from swapi_client import SWAPIClient
 from models.movie import Movie
 from models.species import Species
@@ -225,55 +222,3 @@ def listar_planetas():
                 print("Entrada no valida. Por favor, introduzca un numero o 'q' para salir") 
 
 
-"""
-Estadisticas sobre naves, promedios y moda
-"""
-def Estadisticas_sobre_Naves():
-    import pandas as pd
-    import csv
-    import os 
-
-    #Leer archivo CSV
-    ruta_archivo = os.path.dirname(os.path.abspath(__file__))+"/csv/starships.csv"
-    df = pd.read_csv(ruta_archivo)
-
-    #Capturar estadisticas
-    Hiperimpulsor_promedio = df["hyperdrive_rating"].mean()
-    mglt_promedio = df["MGLT"].max()
-    velocidad_maxima_atmosfera_promedio = df["max_atmosphering_speed"].max()
-    costo_minimo_promedio = df["cost_in_credits"].min()
-    moda_hyperdrive = df["hyperdrive_rating"].mode().iloc[0]
-    moda_mglt = df["MGLT"].mode().iloc[0]
-    moda_velocidadmax = df["max_atmosphering_speed"].mode().iloc[0]
-    moda_costo = df["cost_in_credits"].mode().iloc[0]
-
-    with open(ruta_archivo, mode='r', encoding='utf-8') as file:
-        csv_reader = csv.DictReade(file)
-        
-        #Limpiar pantalla e imprimir los encabezados
-        os.system('cls')
-        print(f'{'Clase de Nave':<30} {'Hiperimpulsor':<15} {'MGLT':<10} {'Velocidad maxima':<20} {'Costo en Creditos':<15}')
-        print('-' * 95)
-
-        #Leer cada fila del archivo CSV
-        for row in csv_reader:
-            clase_nave = row['name']
-            hiperimpulsor = ['hyperdrive_rating']
-            mglt = row['MGLT']
-            velocidad_maxima = row['max_atmosphering_speed']
-            costo = row['costo_in_credits']
-
-            #Imprimir los datos por clase de nave
-            print(f'{clase_nave:<30} {hiperimpulsor:<15} {mglt:<10} {velocidad_maxima:<20} {costo:<15}')
-
-    #Imprimir el promedio y la moda de los datos
-    print(' ')
-    print(f'Promedio de Clasiicacion del Hierimpulsor: {Hiperimpulsor_promedio:.2f}')
-    print(f'La Moda del Hiperimpulsor                 : {moda_hyperdrive}')
-    print(' ')
-    print(f'Maximo MGLT                              : {mglt_promedio}')
-    print(f'La Moda de MGLT                          : {moda_mglt}')
-    print(' ')
-    print(f'Costo Minimo en Creditos                 : {costo_minimo_promedio}')
-    print(f'La Moda del Costo Minimo en Creditos     : {moda_costo}')
-    print(' ')
