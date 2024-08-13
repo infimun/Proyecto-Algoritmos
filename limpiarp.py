8c79c6ca4c255697ada4f456c2734ed5dc43a3e4
SergioDLC793 <sdelacorreia@correo.unimet.edu.ve>
2024-08-12 22:23:38 -0400

    Delete limpiarp.py

--- limpiarp.py
+++
@@ -1,6 +0,0 @@
-import os
-def limpiar_pantalla():
-    if os.name == 'nt':  # Verifica si estamos en Windows
-        os.system('cls')  # Limpia la pantalla
-    else:
-        os.system('clear') # Limpia la pantalla
